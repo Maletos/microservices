5b77462c2b9e0cf37b3eeaf64d5bc854a6700de5
Sergey <sergey257821@yandex.ru>
2023-01-26 17:03:33 +0400

    add tests and resources

--- src/test/java/com/example/firstservice/controller/UserControllerTest.java
+++ src/test/java/com/example/firstservice/controller/UserControllerTest.java
@@ -1,6 +1,5 @@
 package com.example.firstservice.controller;
 
-import com.example.firstservice.Application;
 import org.hamcrest.Matchers;
 import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,26 +9,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK)
 @ExtendWith(SpringExtension.class)
-class UserControllerTest {
+class UserControllerTest extends AbstractTransactionalJUnit4SpringContextTests {
     @Autowired
     private WebApplicationContext webContext;
-
+    private EmbeddedDatabase usersDB;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
     private MockMvc mockMvc;
 
     @BeforeEach
     public void init() {
         mockMvc = MockMvcBuilders.webAppContextSetup(webContext).build();
+        jdbcTemplate.execute("delete from users_schema.users");
+        jdbcTemplate.execute("ALTER SEQUENCE users_schema.users_seq RESTART WITH 1");
     }
+
     @Test
     void createUser() throws Exception {
         JSONObject request = new JSONObject();
@@ -47,6 +55,28 @@ class UserControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(request.toString()));
         resultActions.andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
-                .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.equalTo("User with userName: segorov has been saved in the database with id 1")));
+                     .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.equalTo("User with userName: VasyaM has been saved in the database with id 1")));
+    }
+
+    @Test
+    void getUser() throws Exception {
+        createUser();
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders
+               .get("/user/getUser/{id}", 1)
+                .accept(MediaType.APPLICATION_JSON));
+
+        resultActions.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1));
+    }
+
+    @Test
+    void getUserList() throws Exception {
+        createUser();
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders
+                .get("/user/getUserList")
+                .accept(MediaType.APPLICATION_JSON));
+        resultActions.andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers..size(1));
     }
 }
